a73826314094834cede9638120d427e5352f462b
austinmei5 <austinmei@uchicago.edu>
2022-10-19 00:15:54 +0000

    add subtract

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,27 @@
 class ApplicationController < ActionController::Base
+  def blank_add
+    render({ :template => "calculation_templates/addition_form.html.erb" })
+  end
 
+  def magic_add
+    @first_num = params.fetch("first_num").to_f
+    @second_num = params.fetch("second_num").to_f
+
+    @result = @first_num + @second_num
+
+    render({ :template => "calculation_templates/addition_results.html.erb" })
+  end
+
+  def blank_sub
+    render({ :template => "calculation_templates/subtraction_form.html.erb" })
+  end
+
+  def magic_subtract
+    @first_num = params.fetch("first_num").to_f
+    @second_num = params.fetch("second_num").to_f
+
+    @result = @second_num - @first_num
+
+    render({ :template => "calculation_templates/subtraction_results.html.erb" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get("/add", { :controller => "application", :action => "blank_add" })
+  get("/wizard_add", { :controller => "application", :action => "magic_add" })
+
+  get("/subtract", { :controller => "application", :action => "blank_sub" })
+  get("/wizard_subtract", { :controller => "application", :action => "magic_subtract" })
 end
